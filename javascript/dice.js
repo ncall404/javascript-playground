ab2a47a42d85324f718fab8561f92d6b60d7d20c
Nathan Call <37190397+ncall404@users.noreply.github.com>
2024-08-26 13:46:25 -0600

    Added comments to dice.js and changed roll to use the sides property

--- javascript/dice.js
+++ javascript/dice.js
@@ -1,9 +1,11 @@
+// A side-independent dice class where the amount of sides is determined on object creation.
 class Dice {
     constructor(sides) {
         this.sides = sides;
     }
 
+    // "Rolls the dice" by picking a random number within the amount of sides chosen.
     roll() {
-        return Math.floor(Math.random() * 6) + 1;
+        return Math.floor(Math.random() * this.sides) + 1;
     }
 }
